85f5be8669b79a3b2166c57cb420fd508a879c2b
Nathan Cook <nathancookdev@gmail.com>
2023-03-21 12:46:41 -0500

    Update render.js

--- render.js
+++ render.js
@@ -99,7 +99,7 @@ const renderFunctions = {
         return utils.returnStreamOrCallbackError(err, stream, callback);
       }
       
-      if(response?.setTimeout) {
+      if(!!response?.setTimeout && typeof response.setTimeout === 'function') {
        response.setTimeout(10000); 
       }
 
@@ -163,7 +163,7 @@ const renderFunctions = {
         }
         return utils.returnStreamOrCallbackError(err, stream, callback);
       }
-      if(response?.setTimeout) {
+      if(!!response?.setTimeout && typeof response.setTimeout === 'function') {
        response.setTimeout(10000); 
       }
       
